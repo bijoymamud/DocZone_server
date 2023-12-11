ebe24368764a49752650e786cbb822cd028687ea
bijoymamud <bijoymamud.09@gmail.com>
2023-12-11 09:04:54 +0600

    jwt in adminPannel

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -9,11 +10,40 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const veryfyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
+
+//jwt
+
+app.post('/jwt', (req, res) => {
+  const user = req.body;
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+
+  res.send({ token })
+})
+
+
+
 
 //database connection
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vrgzzke.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,8 +60,64 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-
+    const usersCollection = client.db("doczoneDB").collection("users");
     const doctorCollection = client.db("doczoneDB").collection("doctor");
+    const appoinmentCollection = client.db("doczoneDB").collection("appoinment");
+    const beDoctorCollection = client.db("doczoneDB").collection("beDoctor");
+
+
+
+    //user related apis
+
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await usersCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: 'user already exists' })
+      }
+
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get('/users/admin/:email', veryfyJWT, async (req, res) => {
+
+
+      const email = req.params.email;
+
+      if (req.decoded.email !== email) {
+        res.send({ admin: false })
+
+      }
+      const query = { email: email }
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === 'admin' }
+      res.send(result)
+    })
+
+
+    //making admin
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result)
+    })
+
 
     app.get('/doctor', async (req, res) => {
       const result = await doctorCollection.find().toArray();
@@ -39,9 +125,36 @@ async function run() {
     })
 
 
+    //for delete
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    })
 
 
 
+
+    //inserting appoinment data in dashboard
+
+    app.post('/appoinment', async (req, res) => {
+      const booking = req.body;
+      console.log(booking);
+      const result = await appoinmentCollection.insertOne(booking);
+      res.send(result)
+    })
+
+
+
+    //dedoctor page
+
+    // app.post('/beDoctor', async (req, res) => {
+    //   const info = req.body;
+    //   console.log(info);
+    //   const result = await beDoctorCollection.insertOne(info);
+    //   res.send(result);
+    // });
 
 
 
